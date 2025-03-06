dce968e00c71eb81fdaa2fef4a119e63f6bbc9e4
Subhrojoti <subhrojoti20@gmail.com>
2025-03-06 08:29:16 -0800

    initial commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const login = () => {
+  return (
+    <section>
+      <div className="bg-black h-[100vh] text-white flex items-center justify-center">
+        <div className="bg-yellow-50 text-black w-96 h-[35rem] rounded-lg">
+          Login
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default login;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        black: "#191919",
+      },
+    },
+  },
+  plugins: [],
+};
